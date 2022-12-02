be76e38b23854114e2af7bafa74f0d2362de1cdc
matrix-a11y <qsfyrfdwee@qq.com>
2022-12-02 16:27:08 +0800

    Add AnimBP

--- Source/Gemini_NSE/Public/CloneAnimInstance.h
+++ Source/Gemini_NSE/Public/CloneAnimInstance.h
@@ -4,12 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
-#include "LumineCharacter.generated.h"
-
+#include "LumineCharacter.generated.h.h"
+#include "Charter/LumineCharacter.h"
+#include "CloneAnimInstance.generated.h"
 
 /**
  * 
  */
+/*我是人类冒险家的象征*/
+/*上面的，你是凯瑟林吗*/
 UCLASS(Transient,Blueprintable,BlueprintType)
 class GEMINI_NSE_API UCloneAnimInstance : public UAnimInstance
 {
@@ -18,7 +21,10 @@ public:
 	UCloneAnimInstance();
 	virtual void NativeInitializeAnimation() override;
 	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
-	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	UPROPERTY(EditAnywhere,)
 	ALumineCharacter* Main;
-	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	
+	
+	
+	
 };
